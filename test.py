e66c22959aad4853840f6128a7be4cd96656310e
hlebbatonov <glebplatonov08@gmail.com>
2024-01-19 15:59:50 +0300

    alpha version of game

--- test.py
+++ test.py
@@ -1,10 +1,57 @@
-import keyboard, time
-while True:
-    a = keyboard.read_event()     #Reading the key
-    if a.name == "esc":break      #Loop will break on pressing esc, you can remove that
-    elif a.event_type == "down":  #If any button is pressed (Not talking about released) then wait for it to be released
-        t = time.time()           #Getting time in sec
-        b = keyboard.read_event()
-        while not b.event_type == "up" and b.name == a.name:  #Loop till the key event doesn't matches the old one
-            b = keyboard.read_event()
-        print('Pressed Key "'+ b.name + '" for ' + str(time.time()-t))
+import pygame as pygame
+
+
+def main():
+    screen = pygame.display.set_mode((640, 480))
+    font = pygame.freetype.Font("C:/Windows/Fonts/arial.ttf", 32)
+    clock = pygame.time.Clock()
+    input_box = pygame.Rect(100, 100, 140, 32)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(event.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        screen.fill((30, 30, 30))
+        # Render the current text.
+        txt_surface = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        # Blit the text.
+        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        # Blit the input_box rect.
+        pygame.draw.rect(screen, color, input_box, 2)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    main()
+    pygame.quit()
